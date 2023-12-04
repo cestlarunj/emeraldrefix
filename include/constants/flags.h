4ad3c94b6fa9233e1d0cca53a3c4bfc0b5db2533
Runj <paul.rungette@gmail.com>
2023-12-04 20:06:13 +0100

    added a new section on route 111 to get  items

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1218,10 +1218,11 @@
 #define FLAG_ITEM_SAFARI_ZONE_NORTH_EAST_NUGGET                     0x491
 #define FLAG_ITEM_SAFARI_ZONE_SOUTH_EAST_BIG_PEARL                  0x492
 
-#define FLAG_UNUSED_0x493                                           0x493 // Unused Flag
-#define FLAG_UNUSED_0x494                                           0x494 // Unused Flag
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
-#define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
+#define FLAG_ITEM_ROUTE_111_SMOOTH_ROCK                             0x493
+#define FLAG_ITEM_ROUTE_111_PP_UP                                   0x494
+#define FLAG_ITEM_ROUTE_111_HEART_SCALE                             0x495
+#define FLAG_ITEM_ROUTE_111_YELLOW_DNA                              0x496
+
 #define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
 #define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
